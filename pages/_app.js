c6eaa29669dc2f707358f45376f5401950b1e903
Ludoviko <me@ludoviko.xyz>
2021-06-06 19:50:24 +0100

    Added Plausible through a Next.js plugin. Will it work?

--- pages/_app.js
+++ pages/_app.js
@@ -4,23 +4,25 @@ import 'bootstrap/dist/css/bootstrap.css'
 import Navigation from '../components/nav'
 import Footer from '../components/footer.js'
 import Head from 'next/head'
+import PlausibleProvider from 'next-plausible'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <link rel="icon" href="/RingRingTechSupport.ico" />
-        <meta property="og:title" content="Ludoviko.xyz" key="title" />
-        <meta name="description" content="Ludoviko's little place on the internet" key="desc" />
-        <meta property="og:description" content="Ludoviko's little place on the internet" key="og:desc" />
-        <meta property="og:type" content="website" key="og:type" />
-        <meta property="og:image" content="/RingRingTechSupport.png" key="og:img" />
-        <meta name="keywords" content="Ludoviko, Programming, Minecraft, Game Development, Java, Blog, Kotlin" key="keyw" />
-        <script async defer data-domain="ludoviko.xyz" src="https://plausible.io/js/plausible.js"></script>
-      </Head>
-      <Navigation />
-      <Component {...pageProps} />
-      <Footer />
+      <PlausibleProvider domain="ludoviko.xyz">
+        <Head>
+          <link rel="icon" href="/RingRingTechSupport.ico" />
+          <meta property="og:title" content="Ludoviko.xyz" key="title" />
+          <meta name="description" content="Ludoviko's little place on the internet" key="desc" />
+          <meta property="og:description" content="Ludoviko's little place on the internet" key="og:desc" />
+          <meta property="og:type" content="website" key="og:type" />
+          <meta property="og:image" content="/RingRingTechSupport.png" key="og:img" />
+          <meta name="keywords" content="Ludoviko, Programming, Minecraft, Game Development, Java, Blog, Kotlin" key="keyw" />
+        </Head>
+        <Navigation />
+        <Component {...pageProps} />
+        <Footer />
+      </PlausibleProvider>
     </>
   )
 }
