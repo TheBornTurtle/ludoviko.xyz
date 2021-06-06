f4051c7ce39c245b353ebdd99d4e7a44ab851dd1
Ludoviko <me@ludoviko.xyz>
2021-06-06 20:40:54 +0100

    Modified next.js config for use on netlify

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 module.exports = {
-  async rewrites() {
-      return [
-          {
-              source: '/js/index.js',
-              destination: 'https://plausible.io/js/plausible.js'
-          },
-          {
-              source: '/api/event', // Or '/api/event/' if you have `trailingSlash: true` in this config
-              destination: 'https://plausible.io/api/event'
-          }
+    target = "serverless",
+    async rewrites() {
+        return [
+            {
+                source: '/js/index.js',
+                destination: 'https://plausible.io/js/plausible.js'
+            },
+            {
+                source: '/api/event', // Or '/api/event/' if you have `trailingSlash: true` in this config
+                destination: 'https://plausible.io/api/event'
+            }
       ];
   },
 }
